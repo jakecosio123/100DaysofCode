0241887df21519a7a8db52a85dc003bfd3b81198
Jake <jakecosio123@gmail.com>
2023-01-04 17:15:11 -0600

    day3

---
+++ Day3_TreasureIsland/treasureisland.py
@@ -0,0 +1,49 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+direction = input("You come to a crossroads, do you go left or right?\n").lower()
+
+
+
+if direction == "left":
+    swim_or_wait = input ("After travelling down the road for some time, you come to a river. Type 'swim' to swim across or 'wait' to wait for the ferry.\n").lower()
+    if swim_or_wait == "wait":
+        door = input("After you land safely on the other side, you come to a building with a red, a yellow, and a blue door. Which color door do you choose?\n").lower()
+        if door == "red":
+            print("The door opens and fire shoots out, burning you alive. Game Over.")
+        elif door == "blue":
+            print("As you open the door, wild beasts stampede out and trample you. Game Over.")
+        elif door == "yellow":
+            print("You have found the treasure! You Win!")
+        else:
+            print("Game Over.")
+    else:
+        print("You are attacked by a trout. Game Over.")
+else:
+    print("You fall into a hole. Game Over.")
