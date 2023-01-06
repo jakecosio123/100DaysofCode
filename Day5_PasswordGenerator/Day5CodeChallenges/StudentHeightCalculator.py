ce17e9868b3fadb9236fb61b20ab3c35aea3dc18
jakecosio123 <jakecosio123@gmail.com>
2023-01-06 11:43:28 -0600

    add comments to day 5 student height calculator

--- Day5_PasswordGenerator/Day5CodeChallenges/StudentHeightCalculator.py
+++ Day5_PasswordGenerator/Day5CodeChallenges/StudentHeightCalculator.py
@@ -6,10 +6,15 @@ for n in range(0, len(student_heights)):
 
 
 #Write your code below this row 👇
+
+#set initial variables
 total_height = 0
 num_of_students = 0
 
+#increment the variables for each height input
 for student in student_heights:
     total_height += student
     num_of_students += 1
+
+#prints a rounded average of the input heights
 print(round(total_height/num_of_students))
