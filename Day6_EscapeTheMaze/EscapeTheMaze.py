9f18e8aff7247a1d63d56065b19122505eef9c18
jakecosio123 <jakecosio123@gmail.com>
2023-01-07 00:57:28 -0600

    update day6 project and readme after fixing bug

--- Day6_EscapeTheMaze/EscapeTheMaze.py
+++ Day6_EscapeTheMaze/EscapeTheMaze.py
@@ -5,12 +5,21 @@ def turn_right():
     turn_left()
     turn_left()
 
+#set loop variable to check if stuck in a loop
+loop = 0
 #loops until Reeborg reaches the goal
 while not at_goal():
     if front_is_clear() and wall_on_right():
         move()
+        loop = 0
     elif wall_in_front() and not right_is_clear():
         turn_left()
-    elif right_is_clear():
+        loop = 0
+    #if we do the following 4 times we are stuck in a loop, incrementing the loop variable each time will force it out of the loop by sending it to the else statement
+    elif right_is_clear() and loop < 4:
         turn_right()
         move()
+        loop += 1
+    else:
+        move()
+        loop = 0
