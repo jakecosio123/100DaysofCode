28d209f06f1ea54de4d0f9ecea5486d9f9cddfa4
Jake <jakecosio123@gmail.com>
2023-01-11 15:19:36 -0600

    add 10.1 and 10.2

---
+++ Day10_Calculator/10.1.py
@@ -0,0 +1,9 @@
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    f_name_formatted = f_name.title()
+    l_name_formatted = l_name.title()
+
+    return f"{f_name_formatted} {l_name_formatted}"
+
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
