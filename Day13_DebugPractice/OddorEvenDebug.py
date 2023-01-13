e012b68e3e9a883e7e4f85e634446a94f7a34cb0
Jake <jakecosio123@gmail.com>
2023-01-13 13:27:07 -0600

    13.1 debug practice 1

---
+++ Day13_DebugPractice/OddorEvenDebug.py
@@ -0,0 +1,15 @@
+# number = int(input("Which number do you want to check?"))
+
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+#Code above has = on line 3 instead of ==. Corrected on line 11.
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
