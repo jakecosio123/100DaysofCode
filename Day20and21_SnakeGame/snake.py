d80d2f1639b49eea0eeecb61d3b3a19a6252bb0a
Jake <jakecosio123@gmail.com>
2023-01-23 12:35:18 -0600

    day 20 snake game start

---
+++ Day20and21_SnakeGame/snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for num in range(3):
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(-20*num, 0)
+            self.segments.append(new_segment)
+
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num-1].xcor()
+            new_y = self.segments[seg_num-1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
