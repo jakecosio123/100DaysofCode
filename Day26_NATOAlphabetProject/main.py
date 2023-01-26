9de16d3b1b2aa5c4e38fd617e15c1d4253970306
Jake <jakecosio123@gmail.com>
2023-01-26 11:45:53 -0600

    day 26 nato alphabet project

---
+++ Day26_NATOAlphabetProject/main.py
@@ -0,0 +1,8 @@
+import pandas
+
+nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
+
+word = input("Enter a word: ").upper()
+result = [nato_dict[letter] for letter in word]
+print(result)
