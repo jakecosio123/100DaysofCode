0d03f0ad86da83d3c419053e656f20dd1fd42158
Jake <jakecosio123@gmail.com>
2023-01-27 13:40:37 -0600

    day 27 prac with kwargs

--- Day27_MilesToKilometersConverter/day-27-start/argprac.py
+++ Day27_MilesToKilometersConverter/day-27-start/argprac.py
@@ -7,3 +7,27 @@ def add(*args):
 
 
 print(add(1, 2, 3, 4, 5))
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key,value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+
+my_car = Car(make="Nissan")
+print(my_car.model)
