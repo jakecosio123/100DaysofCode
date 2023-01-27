115b97e5b576fc0f5a7837f3cf9e20992caefcf0
Jake <jakecosio123@gmail.com>
2023-01-27 13:58:18 -0600

    day 27 prac with tkinter and add tkinter reference

--- Day27_MilesToKilometersConverter/day-27-start/tkinterprac.py
+++ Day27_MilesToKilometersConverter/day-27-start/tkinterprac.py
@@ -6,8 +6,25 @@ window.minsize(width=500, height=300)
 
 # label
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24))
-my_label.pack(side="left")
+my_label.pack()
+
+my_label["text"] = "New text"
+my_label.config(text="New Text")
+
+# button
 
 
+def button_clicked():
+    my_label.config(text=screen_input.get())
+
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# entry
+
+screen_input = tkinter.Entry(width=10)
+screen_input.pack()
+screen_input.get()
 
 window.mainloop()
