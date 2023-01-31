32790cccb144d69eda62db90b55ba77bd3f2d98e
Jake <jakecosio123@gmail.com>
2023-01-31 14:59:12 -0600

    day 30 update password manager - add json objects, add search func, and exceptions

--- Day26_NATOAlphabetProject/main.py
+++ Day26_NATOAlphabetProject/main.py
@@ -5,13 +5,14 @@ nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
 
 def nato_converter():
+    word = input("Enter a word: ").upper()
     try:
-        word = input("Enter a word: ").upper()
         result = [nato_dict[letter] for letter in word]
-        print(result)
     except KeyError:
         print("Sorry, only letters in the alphabet please.")
         nato_converter()
+    else:
+        print(result)
 
 
 nato_converter()
--- Day29and30_PasswordManager/main.py
+++ Day29and30_PasswordManager/main.py
@@ -2,9 +2,12 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -30,19 +33,53 @@ def save():
     website = web_entry.get()
     username = username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "username": username,
+            "password": password,
+        }
+    }
     if len(website) == 0 or len(username) == 0 or len(password) == 0:
         messagebox.showinfo(title="Error", message="Please do not leave any fields empty")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\nUsername: {username}\n"
-                                                      f"Password: {password}\nIs it ok to save?")
+                                                              f"Password: {password}\nIs it ok to save?")
         if is_ok:
-            with open("data.txt", mode="a") as data:
-                data.write(f"{website} | {username} | {password}\n")
+            try:
+                with open("data.json", mode="r") as data_file:
+                    data = json.load(data_file)
+
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                data.update(new_data)
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
                 web_entry.delete(0, END)
                 password_entry.delete(0, END)
 
+# --------------------------- Password Search __________________________#
+
+
+def search():
+    website = web_entry.get()
+    try:
+        with open("data.json", mode="r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found")
+    else:
+        if website in data:
+            username = data[website]["username"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Username: {username}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"There are no account details saved for {website}.")
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Password Manager")
@@ -61,18 +98,20 @@ password_label = Label(text="Password: ")
 password_label.grid(column=0, row=3, sticky="E")
 
 web_entry = Entry()
-web_entry.grid(column=1, row=1, columnspan=2, sticky="EW", pady=3)
+web_entry.grid(column=1, row=1, sticky="EW", pady=3, padx=3)
 web_entry.focus()
 username_entry = Entry()
 username_entry.grid(column=1, row=2, columnspan=2, sticky="EW", pady=3)
 username_entry.insert(0, "example_autofill_email@gmail.com")
 password_entry = Entry()
-password_entry.grid(column=1, row=3, columnspan=2, sticky="EW", pady=3)
+password_entry.grid(column=1, row=3, sticky="EW", pady=3, padx=3)
 
-pass_gen_btn = Button(text="Generate Password", command=password_generator)
+web_search_btn = Button(text="Search", command=search)
+web_search_btn.grid(column=2, row=1, sticky="EW", padx=3)
+pass_gen_btn = Button(text="Generate Password", command=password_generator, padx=3)
 pass_gen_btn.grid(column=2, row=3, sticky="EW")
 add_btn = Button(text="Add", command=save)
-add_btn.grid(column=1, row=4, columnspan=2, sticky="EW", pady=3)
+add_btn.grid(column=1, row=4, columnspan=2, sticky="EW", padx=3, pady=3)
 
 
 window.mainloop()
