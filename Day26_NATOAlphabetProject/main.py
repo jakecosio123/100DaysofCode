9986bfee43894c26ade669c044303f7b38a4ca9b
Jake <jakecosio123@gmail.com>
2023-01-31 13:58:55 -0600

    day 30 practice with exceptions - add exceptions to nato alphabet converter

--- Day26_NATOAlphabetProject/main.py
+++ Day26_NATOAlphabetProject/main.py
@@ -3,6 +3,15 @@ import pandas
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
-word = input("Enter a word: ").upper()
-result = [nato_dict[letter] for letter in word]
-print(result)
+
+def nato_converter():
+    try:
+        word = input("Enter a word: ").upper()
+        result = [nato_dict[letter] for letter in word]
+        print(result)
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        nato_converter()
+
+
+nato_converter()
